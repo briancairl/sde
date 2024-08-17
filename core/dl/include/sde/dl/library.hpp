72818317e5580e0d3592581f421aac33ee08239e
Brian Cairl <briancairl@gmail.com>
2024-08-17 19:51:57 -0400

    Adds dynamic library loading/wrappers/tests

--- core/dl/include/sde/dl/library.hpp
+++ core/dl/include/sde/dl/library.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-// C++ Standard Library
-#include <string>
-
 // SDE
 #include "sde/expected.hpp"
 
@@ -11,26 +8,62 @@ namespace sde::dl
 
 struct Error
 {
-  std::string details;
+  const char* details;
 };
+
 
 class Symbol
 {
 public:
-  bool isNull() const { return handle_ == nullptr; }
-  bool isValid() const { return !isNull(); }
+  constexpr explicit Symbol(void* handle) : handle_{handle} {}
+
+  constexpr bool isNull() const { return handle_ == nullptr; }
+  constexpr bool isValid() const { return !isNull(); }
+  constexpr void* handle() const { return handle_; }
 
 private:
   void* handle_ = nullptr;
 };
 
+
+template <typename F> class Function;
+
+
+template <typename R, typename... Args> class Function<R(Args...)>
+{
+public:
+  template <typename... Ts> R operator()(Ts&&... args) const
+  {
+    using FPtr = R (*)(Args...);
+    return reinterpret_cast<FPtr>(symbol_.handle())(std::forward<Ts>(args)...);
+  }
+
+  constexpr explicit Function(Symbol sym) : symbol_{sym} {}
+
+private:
+  Symbol symbol_;
+};
+
+
 class Library
 {
 public:
+  ~Library();
+
+  Library(Library&& other);
+
   expected<Symbol, Error> get(const char* symbol) const;
+
   static expected<Library, Error> load(const char* library_path);
 
+  void swap(Library& other);
+
+  void* handle() const { return handle_; }
+
 private:
+  Library(const Library& other) = delete;
+  constexpr explicit Library(void* handle) : handle_{handle} {}
+
   void* handle_ = nullptr;
 };
 
--- core/dl/src/library.cpp
+++ core/dl/src/library.cpp
@@ -8,5 +8,44 @@
 #include "sde/dl/library.hpp"
 #include "sde/logging.hpp"
 
-namespace sde::game
-{}  // namespace sde::game
+namespace sde::dl
+{
+
+Library::~Library()
+{
+  if (handle_ == nullptr)
+  {
+    return;
+  }
+  dlclose(handle_);
+}
+
+void Library::swap(Library& other) { std::swap(other.handle_, this->handle_); }
+
+Library::Library(Library&& other) { this->swap(other); }
+
+expected<Symbol, Error> Library::get(const char* symbol) const
+{
+  if (handle_ == nullptr)
+  {
+    return make_unexpected(Error{"invalid"});
+  }
+  const auto sym_ptr = dlsym(handle_, symbol);
+  if (sym_ptr == nullptr)
+  {
+    return make_unexpected(Error{dlerror()});
+  }
+  return Symbol{sym_ptr};
+}
+
+expected<Library, Error> Library::load(const char* library_path)
+{
+  const auto lib_ptr = dlopen(library_path, RTLD_LAZY | RTLD_GLOBAL);
+  if (lib_ptr == nullptr)
+  {
+    return make_unexpected(Error{dlerror()});
+  }
+  return Library{lib_ptr};
+}
+
+}  // namespace sde::dl
